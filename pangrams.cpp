6a6e9532414bde8ed34368797b7285fed2210398
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-04-19 21:51:33 +0600

    Pangrams :bug:

--- pangrams.cpp
+++ pangrams.cpp
@@ -10,10 +10,10 @@ using namespace std;
  */
 
 string pangrams(string s) {
-    int arr[26] = { 0 }; // Set everything to 0.
+    int arr[26] = { 0 }; 
     for (char& c : s) {
-        if (c >= 97 && c <= 122) arr[c - 97] = 1; // lowercase
-        else if (c >= 65 && c <= 90) arr[c - 65] = 1; // uppercase
+        if (c >= 97 && c <= 122) arr[c - 97] = 1; 
+        else if (c >= 65 && c <= 90) arr[c - 65] = 1; 
     }
     for (int i = 0; i < 26; i++)
         if (arr[i] == 0) return "not pangram";
